62f284e3b67943bba472b3880a0f8d9f7e256fc5
Toshiki Iga <igapyon@gmail.com>
2017-09-23 18:58:44 +0900

    save

--- src/main/java/blanco/apex/syntaxparser/BlancoApexSyntaxConstants.java
+++ src/main/java/blanco/apex/syntaxparser/BlancoApexSyntaxConstants.java
@@ -21,7 +21,7 @@ package blanco.apex.syntaxparser;
  * @author Toshiki Iga
  */
 public class BlancoApexSyntaxConstants {
-	public static final String VERSION = "v1.0";
+	public static final String VERSION = "v1.1";
 
 	public static String getVersion() {
 		return VERSION;
