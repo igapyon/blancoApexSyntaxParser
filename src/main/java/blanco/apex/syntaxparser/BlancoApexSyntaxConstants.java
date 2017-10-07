c3fd40e870ddc1d76c1d7bd3ce13b4fe598f25fd
Toshiki Iga <igapyon@gmail.com>
2017-10-07 21:51:38 +0900

    trying to switch to Maven repos.

--- src/main/java/blanco/apex/syntaxparser/BlancoApexSyntaxConstants.java
+++ src/main/java/blanco/apex/syntaxparser/BlancoApexSyntaxConstants.java
@@ -21,7 +21,7 @@ package blanco.apex.syntaxparser;
  * @author Toshiki Iga
  */
 public class BlancoApexSyntaxConstants {
-    public static final String VERSION = "v1.1.1";
+    public static final String VERSION = "v1.2";
 
     public static String getVersion() {
         return VERSION;
